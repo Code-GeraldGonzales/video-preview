82dbf158f4591f40d83b2a7369d1209755fe423c
Gerald Gonzales <104032271+Code-GeraldGonzales@users.noreply.github.com>
2022-05-25 12:24:34 -0700

    Add files via upload

---
+++ Video_Preview/script.js
@@ -0,0 +1,50 @@
+console.log("page loaded...");
+
+function playVideo(x) {
+    x.play();
+} 
+
+function stopVideo(x) {
+    x.pause();
+    
+}
+
+function playVideoTwo(y) {
+    y.play();
+} 
+
+function stopVideoTwo(y) {
+    y.pause();
+    
+}
+
+
+
+
+
+
+/*var myVideo=document.getElementById("video1")
+
+function playPause()
+{ 
+if (myVideo.paused) 
+myVideo.play(); 
+else 
+myVideo.pause(); 
+}
+
+
+var myVideoTwo=document.getElementById("video2")
+
+function playPauseTwo()
+{ 
+if (myVideoTwo.paused) 
+myVideoTwo.play(); 
+else 
+myVideoTwo.pause(); 
+}
+
+*/
+
+
+
